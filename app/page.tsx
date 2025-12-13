dc902c33faad4aed13ba1645e48e7931c4dc1f8c
Kalana Abeysundara <knbabeysundara2003.it@gmail.com>
2025-12-13 23:55:55 +0530

    Project initialized

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
           <Hero />
           <main className="flex-1 flex flex-col gap-6 px-4">
             <h2 className="font-medium text-xl mb-4">Next steps</h2>
-            {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+            {hasEnvVars() ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
           </main>
         </div>
 
